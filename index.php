cb7e29fe6e80dd987b981a7f4d23cd8ce49d0703
trudly <trudly@outlook.com>
2017-08-24 02:51:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,12 @@ shell_exec('chmod +x pure');
 header('Content-Type: application/json');
 parse_str($_SERVER['QUERY_STRING']);
 $cmd = "./pure $b $f $t";
-$output = shell_exec($cmd);
-if(!$output)
-    http_response_code(204);
-print_r (json_encode(explode("\n",trim($output))));
+echo '<pre>';
+echo $cmd;
+echo shell_exec('ls -la');
+echo '</pre>';
+//$output = shell_exec($cmd);
+//if(!$output)
+//    http_response_code(204);
+//print_r (json_encode(explode("\n",trim($output))));
 ?>
