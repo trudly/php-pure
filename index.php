5172c8de166a435e85d6ff25863be0f6301383c1
trudly <trudly@outlook.com>
2017-08-24 03:02:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 shell_exec('chmod +x pure');
+shell_exec('chmod +x pri');
 header('Content-Type: application/json');
 parse_str($_SERVER['QUERY_STRING']);
 $cmd = "./pure $b $f $t";
 echo '<pre>';
 echo $cmd;
 echo shell_exec('ls -la');
+echo shell_exec('./pri 2>&1');
 echo '</pre>';
 //$output = shell_exec($cmd);
 //if(!$output)
